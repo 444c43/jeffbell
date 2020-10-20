1b76cd4e0100feddf7e1d3cb91c5f9f46acf2bfa
DL <444c43@gmail.com>
2020-10-20 12:11:21 -0400

    remove event-date js

--- assets/js/minor.js
+++
@@ -1,4 +0,0 @@
-var today = new Date(+new Date + 12096e5);
-var date = (today.getMonth()+1) + '/' + today.getDate() + '/' + today.getFullYear()
-
-document.getElementById('event-date').value = date;
